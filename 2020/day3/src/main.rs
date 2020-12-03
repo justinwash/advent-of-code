86e042b7e93fb5cbe57c7735a8ffad21e3104af5
Justin Wash <justin@Justins-iPro.attlocal.net>
2020-12-03 12:31:41 -0600

    day3 solution

--- 2020/day3/src/main.rs
+++ 2020/day3/src/main.rs
@@ -15,18 +15,43 @@ fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
         .collect()
 }
 
+fn locate_trees(input: &Vec<String>, r: usize, d: usize) -> u128 {
+    let mut x = 0;
+    let mut y = 0;
+
+    let mut trees = 0;
+
+    while y < input.len() {
+        if input[y].chars().nth(x % input[y].len()).unwrap() == '#' {
+            trees += 1;
+        }
+        x += r;
+        y += d;
+    }
+
+    trees
+}
+
 fn part_one() {
     print_time!("part 1");
 
     let input = lines_from_file("input");
+    let trees = locate_trees(&input, 3, 1);
 
-    println!("part 1 result: {}", "result");
+    println!("part 1 result: {}", trees);
 }
 
 fn part_two() {
     print_time!("part 2");
 
-    println!("part 2 result: {}", "result")
+    let input = lines_from_file("input");
+    let result: u128 = locate_trees(&input, 1, 1)
+        * locate_trees(&input, 3, 1)
+        * locate_trees(&input, 5, 1)
+        * locate_trees(&input, 7, 1)
+        * locate_trees(&input, 1, 2);
+
+    println!("{}", result)
 }
 
 fn main() {

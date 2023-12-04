8eba2ef2021d2c4f404e51cbba4c370a06a52f24
justin wash <justin.wash.ext@bayer.com>
2023-12-04 13:03:03 -0600

    day1part2

--- 2023/day1/day1.cpp
+++ 2023/day1/day1.cpp
@@ -3,19 +3,50 @@ using namespace std;
 
 #include <fstream>
 #include <iterator>
-#include <regex>
+#include <map>
+
+std::string extractNumerals(const std::string& input) {
+  std::map<std::string, std::string> numericWords = {
+      {"zero", "0"},  {"one", "1"},  {"two", "2"}, {"three", "3"},
+      {"four", "4"},  {"five", "5"}, {"six", "6"}, {"seven", "7"},
+      {"eight", "8"}, {"nine", "9"}};
+
+  std::string result;
+
+  for (size_t i = 0; i < input.size(); ++i) {
+    if (!isalpha(input[i])) {
+      result += input[i];
+    }
+
+    else {
+      std::string currentWord;
+
+      for (size_t j = i; j < input.size(); ++j) {
+        currentWord += std::tolower(input[j]);
+
+        auto it = numericWords.find(currentWord);
+        if (it != numericWords.end()) {
+          result += it->second;
+          break;
+        }
+      }
+    }
+  }
+
+  return result;
+}
 
 ifstream input("input.txt");
 
 int main() {
-  regex nonNumeric("[^0-9]");
   int result = 0;
 
   for (string line; getline(input, line);) {
-    string numericLine = regex_replace(line, nonNumeric, "");
+    string numericLine = extractNumerals(line);
     result += stoi(string(1, numericLine[0]) + string(1, numericLine.back()));
   }
 
+  cout << "Result: \n";
   cout << result;
   return 0;
 }
